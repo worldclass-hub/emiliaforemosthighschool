9675ff970b1fe9a9c5d12fef44537baa436251f0
worldclass-hub <worldbank2120@gmail.com>
2026-02-02 06:42:18 +0100

    emilia

--- invoice_maker/settings.py
+++ invoice_maker/settings.py
@@ -95,25 +95,25 @@ WSGI_APPLICATION = 'invoice_maker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.environ.get('PGDATABASE'),
-#             'USER': os.environ.get('PGUSER'),
-#             'PASSWORD': os.environ.get('PGPASSWORD'),
-#             'HOST': os.environ.get('PGHOST'),
-#             'PORT': os.environ.get('PGPORT', '5432'),
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('PGDATABASE'),
+            'USER': os.environ.get('PGUSER'),
+            'PASSWORD': os.environ.get('PGPASSWORD'),
+            'HOST': os.environ.get('PGHOST'),
+            'PORT': os.environ.get('PGPORT', '5432'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
